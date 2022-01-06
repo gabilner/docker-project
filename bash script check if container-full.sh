b3090aa702ed9192f4cf5feb56d1b867863cbe9d
gabilner <gabilner@gmail.com>
2022-01-06 10:36:40 +0200

    Update bash script check if container-full.sh

--- bash script check if container-full.sh
+++ bash script check if container-full.sh
@@ -8,14 +8,43 @@ if [ "$CONTAINER_NAME_NEW" != "$CONTAINER_NAME" ]
 then
 echo "could not found container $CONTAINER_NAME..."
 docker run --name dockr-alpine-vm -d -v /home/jenkins/dockerpro/dockeimg/volumepath:/home/git docker-alpine01 top
+rm -rf /home/jenkins/dockerpro/dockeimg/volumepath/*
+
+###########################   run script in the docker container ##############
+docker exec -d dockr-alpine-vm git clone https://github.com/gabilner/docker-project.git /home/git
+runtime="10 second"
+endtime=$(date -ud "$runtime" +%s)
+
+mytime=$(date -u +%s)
+echo $mytime
+echo $endtime
+count=0
+while [ $(date -u +%s) -le $endtime ]
+do
+    if [ $count -eq 0  ]
+    then
+        
+        docker exec -d dockr-alpine-vm python3 /home/gitpyscript.py
+        count=1
+
+    fi
+   # echo "Time Now: `date +%H:%M:%S`"
+   # echo "Sleeping for 10 seconds"
+   # sleep 11
+   mytime=$(date -u +%s)
+    echo "$mytime this mu time"
+    echo "$endtime this is endtime"
+if [ $endtime -lt $mytime   ]
+    then
+        echo "THE SCRIPT TAKE MORE THEN 10 SECONDS"
+        exit 1
+    fi
+done
 
 
 
 
-
-
-
-
+#################################################################################
 
 else
 echo " there is already container with that name"
